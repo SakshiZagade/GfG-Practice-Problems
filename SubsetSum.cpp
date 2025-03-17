1adad005f70bcb68e03e137ef136b81f0629e344
Sakshi Zagade <123815646+SakshiZagade@users.noreply.github.com>
2025-03-17 23:34:27 +0530

    SubsetSum.cpp

---
+++ SubsetSum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1, false));
+
+        // If sum is 0, we can always achieve it with an empty subset
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+};
